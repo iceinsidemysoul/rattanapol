547b53c360c292bb0329b0d5bd727fe092031b53
iceinsidemysoul <sompong.paramon@gmail.com>
2017-03-14 09:40:17 +0700

    before-restructured

--- index.php
+++ index.php
@@ -5,16 +5,16 @@
 		while(have_posts()): the_post(); ?>
 		<article class="post">
 			<div class="row">
-				<div class="col-md-4"><?php the_post_thumbnail('custom-size'); ?></div>
-				<div class="col-md-8"><h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2></div>
+				<div class="col-md-4"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('custom-size'); ?></div>
+				<div class="col-md-8"><h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2><?php the_excerpt();?></div>
 			</div>
 		</article>
 		<?php endwhile;?>
-		<div class="nav-previous"><?php next_posts_link( 'Older posts' ); ?></div>
+		<div class="nav-previous"><?php next_posts_link( 'ข่าวก่อนหน้า' ); ?></div>
  
  
  
-		<div class="nav-next"><?php previous_posts_link( 'Newer posts' ); ?></div>
+		<div class="nav-next"><?php previous_posts_link( 'ข่าวถัดไป' ); ?></div>
  
 	<?php
 		else:
